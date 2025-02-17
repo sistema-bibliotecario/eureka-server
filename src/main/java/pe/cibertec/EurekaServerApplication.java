105e9c5560217a406efae29d2eb33241231a190b
Ramiro Blas <ramiro060694@gmail.com>
2025-02-17 15:47:55 -0500

    feat: se agrego mensaje de inicio

--- src/main/java/pe/cibertec/EurekaServerApplication.java
+++ src/main/java/pe/cibertec/EurekaServerApplication.java
@@ -10,7 +10,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-		System.out.println("Eureka Server started");
+		System.out.println("eureka-server inicio correctamente");
 	}
 
 }
